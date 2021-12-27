c141a3db6c1bc0d67f56f393d7d30291a1ca4102
Ronaldo Muniz Jr <ronaldoomjunior@gmail.com>
2021-12-27 18:19:47 -0300

    Criação do projeto

---
+++ src/components/TableArea/index.tsx
@@ -0,0 +1,16 @@
+import * as C from './styles';
+
+export const TableArea = () => {
+    return (
+        <C.Table>
+            <thead>
+                <tr>
+
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </C.Table>
+    );
+}
