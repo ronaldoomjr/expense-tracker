a5f965a6b332cc474020b7c7c5912e2fd66c8a60
Ronaldo Muniz Jr <ronaldoomjunior@gmail.com>
2021-12-29 08:36:58 -0300

    Implementando área de informações

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,22 @@ import { items } from './data/items';
 import { getCurrentMonth } from './helpers/dateFilter';
 import { filterListByMonth } from './helpers/dateFilter';
 import { TableArea } from './components/TableArea';
+import { InfoArea } from './components/InfoArea';
 
 const App = () => {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
 
   useEffect(() => {
     setFilteredList(filterListByMonth(list, currentMonth));
   }, [list, currentMonth]);
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
 
   return (
     <C.Container>
@@ -24,7 +31,12 @@ const App = () => {
       </C.Header>
       <C.Body>
 
-        {/* Área de informações */}
+        <InfoArea
+          currentMonth={currentMonth}
+          onMonthChange={handleMonthChange}
+          income={income}
+          expense={expense}
+        />
 
         {/* Área de inserção */}
 
